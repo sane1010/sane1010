944ebd13e766c9257f5bb772fec8d9cad9a029a0
sane1010 <salihyigitefe@gmail.com>
2025-06-30 02:03:16 +0300

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,14 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return input("ne seversin?")
+
+print("aaa bende nigga çok severim")
+
+input("peki nigger sever misin?")
+
+print("bende çok severim")
+
+app.run()
